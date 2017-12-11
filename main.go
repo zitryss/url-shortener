7fd3efcec54bcd756d81b222fee568daef3f2b9f
Eugene Russkikh <zitryss@gmail.com>
2017-12-11 23:31:07 +0100

    Implement URL shortener

--- main.go
+++ main.go
@@ -1,15 +1,46 @@
 package main
 
 import (
+	"crypto/rand"
 	"fmt"
 	"net/http"
+	"sync"
 )
 
+var urls = make(map[string]string)
+var m = sync.Mutex{}
+
 func main() {
-	http.HandleFunc("/", hello)
+	http.HandleFunc("/", handler)
 	http.ListenAndServe(":8080", nil)
 }
 
-func hello(rw http.ResponseWriter, r *http.Request) {
-	fmt.Fprint(rw, "Hello, 世界")
+func handler(resp http.ResponseWriter, req *http.Request) {
+	switch req.Method {
+	case "POST":
+		longURL := req.FormValue("url")
+		shortURL := "http://localhost:8080/" + hashID(8)
+		m.Lock()
+		urls[shortURL] = longURL
+		m.Unlock()
+		fmt.Fprintln(resp, shortURL)
+	case "GET":
+		shortURL := "http://localhost:8080" + req.URL.Path
+		m.Lock()
+		longURL, ok := urls[shortURL]
+		m.Unlock()
+		if !ok {
+			http.Error(resp, "Page not found.", http.StatusNotFound)
+			return
+		}
+		http.Redirect(resp, req, longURL, http.StatusFound)
+	default:
+		http.Error(resp, "Method is not supported.", http.StatusInternalServerError)
+	}
+}
+
+func hashID(len int) string {
+	b := make([]byte, len/2)
+	rand.Read(b)
+	return fmt.Sprintf("%x", b)
 }
