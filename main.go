82a5b402fbdc56eef9ae69c34128c287a2988f23
Eugene Russkikh <zitryss@gmail.com>
2017-12-11 01:40:15 +0100

    Implement a simple web server

--- main.go
+++ main.go
@@ -2,8 +2,14 @@ package main
 
 import (
 	"fmt"
+	"net/http"
 )
 
 func main() {
-	fmt.Println("Hello, 世界")
+	http.HandleFunc("/", hello)
+	http.ListenAndServe(":8080", nil)
+}
+
+func hello(rw http.ResponseWriter, r *http.Request) {
+	fmt.Fprint(rw, "Hello, 世界")
 }
